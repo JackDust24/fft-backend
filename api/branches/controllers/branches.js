62d142f992b3774493bb530440edceb342087cdd
Jason Whittaker <jason_bournemouth@yahoo.co.uk>
2021-03-16 10:55:55 +0700

    changes to branches - add mail

--- api/branches/controllers/branches.js
+++ api/branches/controllers/branches.js
@@ -13,7 +13,11 @@ module.exports = {
         const {
            data,
            clientId,
-           userId
+           userId,
+           username,
+           user_email,
+           sales_rep_email,
+           membership_premium
         } = ctx.request.body
 
         console.log("Backend called = ", ctx.request.body)
@@ -26,6 +30,8 @@ module.exports = {
 
         console.log("Backend called clientId ", clientId)
         console.log("Backend called userId ", userId)
+        console.log("Backend called membership_premium ", membership_premium)
+
 
 
        let created_date = new Date();
@@ -58,6 +64,7 @@ module.exports = {
         let Discount_alt = data.discount;
         let client = clientId;
         let branch_user = userId;
+        let premium_membership = membership_premium
         // let branch_logo = data.logo;
         // let main_image = 'Have to agree the docs are poor. Knowing how to upload a file from React to a new entry into a content type has got me stumped all day. Still stuck.'
 
@@ -86,6 +93,7 @@ module.exports = {
             branch_description_long,
             Discount_alt,
             branch_user,
+            premium_membership,
                        // Fair_pricing,
             // English_speaking,
             // Parking,
@@ -94,6 +102,47 @@ module.exports = {
         }
 
         console.log("Create entity")
+        console.log("Email went to - " + user_email);
+
+        // The colour we will use.
+        var fftGreen = "rgba(28, 219, 104)"; 
+    
+        try {
+          const send = await strapi.plugins["email"].services.email.send({
+            to: user_email,
+            cc: sales_rep_email,
+            bcc: "staff@ffthai.com",
+            subject: `Hello ${username}!  ${branch_name} has been created.`,
+            text: "Hello there",
+            html: `<h1 style="color:${fftGreen};">Hello ${username},</h1>
+                  <p></p>
+                  <p>Am writing to confirm that your branch <strong>${branch_name}</strong>, which has been updated onto our system.</p>
+                  <p></p>
+                  <p>Please access the <strong>Accounts</strong> page, followed by <strong>Branches</strong> to access your Branch and edit details.</p>
+                 
+                  <p>Thank you again for joining us at Foreigner Friendly Thailand.</p>
+                  <p></p>	
+                  <p>Best regards,</p>
+                  <p></p>
+                  <h5>FFThai Support</h5>
+                  <p></p>
+                  <h2 style="color:${fftGreen};font-size:24px;">With Foreigner Friendly Thailand, everyone gets what they want!</h2>`,
+            //   <img src="https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing"/>`,
+            //   attachments: [
+            //     {
+            //       filename: 'earlybed_88945d7457.jpg',
+            //       path: path.join(
+            //         __dirname + '/../../../public/uploads/earlybed_88945d7457.jpg'
+            //       ),
+            //       cid: 'https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing'
+            //     }
+            //   ]
+          });
+    
+          console.log("send", send);
+        } catch (err) {
+          console.log("Problem with email for branch - ", err);
+        }
 
         const entity = await strapi.services.branches.create(entry);
         return sanitizeEntity(entity, { model: strapi.models.branches })
--- api/clients/controllers/clients.js
+++ api/clients/controllers/clients.js
@@ -1,5 +1,7 @@
-'use strict';
+"use strict";
 const { sanitizeEntity } = require("strapi-utils");
+
+const launchDate = '2021-06-01T00:00:00.527Z';
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -7,109 +9,147 @@ const { sanitizeEntity } = require("strapi-utils");
  */
 
 module.exports = {
+  create: async (ctx) => {
+    const {
+      business_name,
+      business_display_name,
+      client_contact,
+      client_email,
+      franchise,
+      sales_rep,
+      sales_rep_email,
+      business_category,
+      business_sub_category,
+      terms,
+      setup_by,
+      client_id,
+      discount_info,
+      package_type,
+      lengthOfPackage,
+      startDate,
+    } = ctx.request.body;
 
-    create: async (ctx) => {
-        const {
-            business_name,
-            business_display_name,
-            client_contact,
-            client_email,
-            franchise,
-            sales_rep,
-            sales_rep_email,
-            business_category,
-            business_sub_category,
-            terms,
-            setup_by,
-            client_id,
-            discount_info
-        } = ctx.request.body
+    //Check if the data is proper 2
+    console.log("clients.create business_name ", business_name);
+    console.log("clients.create business_display_name ", business_display_name);
+    console.log("clients.create client_contact ", client_contact);
+    console.log("clients.create client_email ", client_email);
+    console.log("clients.create sales_rep ", sales_rep);
+    console.log("clients.create client_id ", client_id);
+    console.log("clients.create sales_rep_email ", sales_rep_email);
+    console.log("clients.create business_category ", business_category);
+    console.log("clients.create business_sub_category ", business_sub_category);
+    console.log("clients.create terms ", terms);
+    console.log("clients.create discount_info ", discount_info);
+    console.log("clients.create package_type ", package_type);
+    console.log("clients.create lengthOfPackage ", discount_info);
+    console.log("clients.create startDate ", discount_info);
 
-       
-        //Check if the data is proper 2
-        console.log("clients.create cart ", business_name)
-        console.log("clients.create cart ", business_display_name)
-        console.log("clients.create cart ", client_contact)
-        console.log("clients.create cart ", client_email)
-        console.log("clients.create cart ", franchise)
-        console.log("clients.create cart ", sales_rep)
-        console.log("clients.create cart ", client_id)
-        console.log("clients.create cart ", sales_rep_email)
-        console.log("clients.create cart ", business_category)
-        console.log("clients.create cart ", business_sub_category)
-        console.log("clients.create cart ", terms)
-        console.log("clients.create cart ", discount_info)
+    console.log("clients.create ctx.request.body ", ctx.request.body);
 
-        console.log("clients.create ctx.request.body ", ctx.request.body)
+    console.log(typeof setup_by);
 
-         console.log(typeof setup_by)
-  
-          // Create Internal ID
-          const randomString = (length) => {
-            var emptyString = "";
-            var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ";
-      
-            while (emptyString.length < length) {
-                emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
-            }
-            
-            return emptyString;
-          }; 
+    // Create Internal ID
+    const randomString = (length) => {
+      var emptyString = "";
+      var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ";
 
-          const randomNumber = (length) => {
-            var emptyString = "";
-            var alphabet = "0123456789";
-      
-            while (emptyString.length < length) {
-                emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
-            }
-            
-            return emptyString;
-          };
-      
-          const randomNum = randomNumber(5);
-          const randomStr = randomString(3);
+      while (emptyString.length < length) {
+        emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+      }
 
-          const InternalID = `FFTCLI-${randomNum}${randomStr}A`;
-          console.log("******** Check bus ID - " + InternalID);
+      return emptyString;
+    };
 
-        // DO a double check here:
-        const client_user = client_id
+    const randomNumber = (length) => {
+      var emptyString = "";
+      var alphabet = "0123456789";
+
+      while (emptyString.length < length) {
+        emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
+      }
+
+      return emptyString;
+    };
+
+    const randomNum = randomNumber(5);
+    const randomStr = randomString(3);
+
+    // Set properties for adding entry
+    const InternalID = `FFTCLI-${randomNum}${randomStr}A`;
+    console.log("******** Check bus ID - " + InternalID);
+
+    const client_user = client_id;
+
+    // Deal with membership packages for the client
+    let premium_membership = false
+    if (package_type === "Premium") {
+      premium_membership = true
+      console.log("******** premium_membership true - ", + premium_membership);
+
+    }
+
+    // Work out membership start
+    let membership_start;
+    let passedInDate = Date.parse(startDate);
+    let fftSetDate = Date.parse(launchDate);
+    if (passedInDate > fftSetDate) {
+      membership_start = Date(startDate);
+      console.log("Chosen date is after launch date ", membership_start);
+ 
+    } else {
+      membership_start = new Date(launchDate);
+      console.log("Chosen date is before launch date ", membership_start);
+
+    }
+
+    // Work out membership end
+    // Work out date
+    let membership_expiry = new Date(membership_start);
+    console.log("Date - ", membership_expiry.toLocaleDateString());
+    membership_expiry.setMonth(membership_expiry.getMonth() + lengthOfPackage);
+    console.log("New Date - ", membership_expiry.toLocaleDateString());
+ 
+    console.log("Membership Expiry ", membership_expiry);
 
 
-        const entry = {
-            business_name,
-            business_display_name,
-            client_contact,
-            client_email,
-            franchise,
-            sales_rep,
-            setup_by,
-            client_user,
-            terms,
-            sales_rep_email,
-            business_category,
-            business_sub_category,
-            discount_info,
-            InternalID
-        }
+    const entry = {
+      business_name,
+      business_display_name,
+      client_contact,
+      client_email,
+      franchise,
+      sales_rep,
+      setup_by,
+      client_user,
+      terms,
+      sales_rep_email,
+      business_category,
+      business_sub_category,
+      discount_info,
+      InternalID,
+      premium_membership,
+      membership_start,
+      membership_expiry
+    };
 
-        let userEmail = client_email;
-        let username = setup_by;
+    // Set properties for email
+    let userEmail = client_email;
+    let username = setup_by;
 
-        console.log("Email went to - " + userEmail);
+    console.log("Email went to - " + userEmail);
 
-        // The colour we will use.
-        var fftGreen = "rgba(28, 219, 104)"; 
-    
-        try {
-          const send = await strapi.plugins["email"].services.email.send({
-            to: userEmail,
-            cc: sales_rep_email,
-            bcc: "staff@ffthai.com",
-            subject: `Hello ${username}!  ${business_name} has been created.`,
-            text: "Hello there",
-            html: `<h1 style="color:${fftGreen};">Hello ${username},</h1>
+    // The colour we will use.
+    var fftGreen = "rgba(28, 219, 104)";
+
+    try {
+      const send = await strapi.plugins["email"].services.email.send({
+        to: userEmail,
+        cc: sales_rep_email,
+        bcc: "staff@ffthai.com",
+        subject: `Hello ${username}!  ${business_name} has been created.`,
+        text: "Hello there",
+        html: `<h1 style="color:${fftGreen};">Hello ${username},</h1>
                   <p></p>
                   <p></strong>Ref: InternalID: <strong>${InternalID}</strong></p>
                   <p></p>
@@ -124,26 +164,24 @@ module.exports = {
                   <h5>FFThai Support</h5>
                   <p></p>
                   <h2 style="color:${fftGreen};font-size:24px;">With Foreigner Friendly Thailand, everyone gets what they want!</h2>`,
-            //   <img src="https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing"/>`,
-            //   attachments: [
-            //     {
-            //       filename: 'earlybed_88945d7457.jpg',
-            //       path: path.join(
-            //         __dirname + '/../../../public/uploads/earlybed_88945d7457.jpg'
-            //       ),
-            //       cid: 'https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing'
-            //     }
-            //   ]
-          });
-    
-          console.log("send", send);
-        } catch (err) {
-          console.log("Problem with email business - ", err);
-        }
-    
-        const entity = await strapi.services.clients.create(entry);
-        return sanitizeEntity(entity, { model: strapi.models.clients })
+        //   <img src="https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing"/>`,
+        //   attachments: [
+        //     {
+        //       filename: 'earlybed_88945d7457.jpg',
+        //       path: path.join(
+        //         __dirname + '/../../../public/uploads/earlybed_88945d7457.jpg'
+        //       ),
+        //       cid: 'https://drive.google.com/file/d/1MhXXE2qfP6NIzIJ9CAae42eaqHm0NrOi/view?usp=sharing'
+        //     }
+        //   ]
+      });
 
+      console.log("send", send);
+    } catch (err) {
+      console.log("Problem with email business - ", err);
     }
 
+    const entity = await strapi.services.clients.create(entry);
+    return sanitizeEntity(entity, { model: strapi.models.clients });
+  },
 };
